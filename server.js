9abbcd9e114e68a853700c4d8f79cd0873c39e5c
Archisman <banikarchisman23@gmail.com>
2023-06-11 19:12:32 +0530

    Third commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const app = express();
 
 app.use(cors());
 
